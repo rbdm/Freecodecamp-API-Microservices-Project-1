91d7651cbad89848baa0d2aeb35043a07e772808
Glitch (glossy-check) <none>
2018-08-24 12:33:42 +0000

    😎🍥 Updated with Glitch

---
+++ myApp.js
@@ -0,0 +1,17 @@
+/*var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+
+var date_string;
+
+var output = new Date(date_string);
+
+console.log(output);
+
+app.get('/api/', (req,res) => {
+  res.json(output);
+});*/
